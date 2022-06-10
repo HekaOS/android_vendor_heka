eab72806e9c53f993c0a9c66513e9629298d5809
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-06-11 01:32:24 +0200

    vendor: Drop Lineage charger

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,15 +1,6 @@
 # Inherit common mobile Heka stuff
 $(call inherit-product, vendor/heka/config/common.mk)
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-ifneq ($(WITH_LINEAGE_CHARGER),false)
-PRODUCT_PACKAGES += \
-    lineage_charger_animation
-endif
-
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
