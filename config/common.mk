tequilaOS/platform_vendor_tequila

vendor: config: sepolicy: import flipendo/turbo_adapter sepolicy
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

config/common.mk
@@ -172,6 +172,8 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, vendor/tequila/config/version.mk)
 
+$(call inherit-product, vendor/tequila/config/sepolicy.mk)
+
 $(call inherit-product, vendor/google/gms/config.mk)
 
 $(call inherit-product, vendor/google/pixel/config.mk)

HekaOS/android_vendor_heka

config/common.mk
@@ -169,5 +169,8 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, vendor/heka/config/version.mk)
 
+# Import flipendo/turbo_adapter sepolicy
+$(call inherit-product, vendor/heka/config/sepolicy.mk)
+
 # Inherit from GMS product config
 $(call inherit-product, vendor/gms/gms_full.mk)