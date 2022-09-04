tequilaOS/platform_vendor_tequila

vendor: Import Pixel bootanimation
Change-Id: Id32cc5e859d5930e5cd81f9d8ab375cbec947311

config/bootanimation.mk
@@ -1,0 +1,13 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/tequila/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/tequila/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/tequila/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif

config/common.mk
@@ -193,6 +193,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/tequila/overlay/common
 
+$(call inherit-product, vendor/tequila/config/bootanimation.mk)
+
 $(call inherit-product, vendor/tequila/config/version.mk)
 
 $(call inherit-product, vendor/tequila/config/sepolicy.mk)

HekaOS/android_vendor_heka

config/bootanimation.mk
@@ -1,0 +1,13 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/heka/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/heka/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/heka/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif

config/common.mk
@@ -167,6 +167,8 @@ PRODUCT_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     NetworkStackOverlay
 
+$(call inherit-product, vendor/heka/config/bootanimation.mk)
+
 $(call inherit-product, vendor/heka/config/version.mk)
 
 # Import flipendo/turbo_adapter sepolicy